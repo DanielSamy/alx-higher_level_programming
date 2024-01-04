0af69c459ba35ddf23949cc934924871ddec839a
DanielSamy <danielsamy2002@gmail.com>
2024-01-04 03:13:59 -0800

    Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,11 +3,10 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
-    if a < b:
+    if a > b:
+        return sub(a, b)
+    else:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-    else:
-        return(sub(a, b))
-    return 0
+    return c
