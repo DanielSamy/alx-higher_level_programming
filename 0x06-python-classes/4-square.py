1b9373f787c565e3bcbc17813e3c2b9058f8f89f
DanielSamy <danielsamy2002@gmail.com>
2024-01-23 06:02:57 -0800

    python-classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+class Square:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Property setter to set size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
