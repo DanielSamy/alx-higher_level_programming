52887361d32533bde06bfdfa4727e4f9f0b1fc34
DanielSamy <danielsamy2002@gmail.com>
2024-01-23 06:11:34 -0800

    python-classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,8 +3,13 @@
 
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
@@ -25,6 +30,7 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
